a6b8b34618fcd900938971cf1286b3c491ee3dc9
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2021-03-17 13:33:13 +0100

    Added functionality

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # chucknorisjoke
  A web app that generate random Chuck Norris jokes.
+
+Here is how the app works:
+
+1. The input on the page take the number of jokes the user wants.
+
+2. The number is used in making a HTTPRequest to the get the jokes.
+
+3. The jokes is displayed on the webpage.
+
+Live URL =
---
+++ app.js
@@ -0,0 +1,34 @@
+const jokesBtn = document.querySelector('.get-jokes');
+
+jokesBtn.addEventListener('click', getJokes);
+
+function getJokes (e) {
+    const number = document.querySelector('input').value;
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+
+    xhr.onload = function () {
+        if (this.status === 200) {
+            const response = JSON.parse(this.responseText);
+
+            let output = '';
+
+            if (response.type === 'success') {
+                response.value.forEach(function(joke) {
+                    output +=  `<li>${joke.joke}</li>`;
+                })
+            } else {
+                output += 'something went wrong';
+            }
+
+            document.querySelector('.jokes').innerHTML = output;
+        }
+    }
+
+    xhr.send();
+
+
+    e.preventDefault();
+}
